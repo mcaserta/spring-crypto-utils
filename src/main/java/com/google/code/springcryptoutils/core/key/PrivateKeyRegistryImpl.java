af2ec318e449425b499b970e37c6a0054c2f0c9f
Mirko Caserta <mirko.caserta@gmail.com>
2010-02-11 11:57:34 +0000

    - implemented a cache for the keys

--- src/main/java/com/google/code/springcryptoutils/core/key/PrivateKeyRegistryImpl.java
+++ src/main/java/com/google/code/springcryptoutils/core/key/PrivateKeyRegistryImpl.java
@@ -5,16 +5,27 @@ import com.google.code.springcryptoutils.core.keystore.KeyStoreRegistry;
 
 import java.security.KeyStore;
 import java.security.PrivateKey;
+import java.util.HashMap;
+import java.util.Map;
 
 public class PrivateKeyRegistryImpl implements PrivateKeyRegistry {
 
     private KeyStoreRegistry keyStoreRegistry;
+
+    private Map<CacheKey, PrivateKey> cache = new HashMap<CacheKey, PrivateKey>();
 
     public void setKeyStoreRegistry(KeyStoreRegistry keyStoreRegistry) {
         this.keyStoreRegistry = keyStoreRegistry;
     }
 
     public PrivateKey get(KeyStoreChooser keyStoreChooser, PrivateKeyChooser privateKeyChooser) {
+        CacheKey cacheKey = new CacheKey(keyStoreChooser.getKeyStoreName(), privateKeyChooser.getAlias());
+        PrivateKey retrievedPrivateKey = cache.get(cacheKey);
+
+        if (retrievedPrivateKey != null) {
+            return retrievedPrivateKey;
+        }
+
         KeyStore keyStore = keyStoreRegistry.get(keyStoreChooser);
 
         if (keyStore != null) {
@@ -24,12 +35,53 @@ public class PrivateKeyRegistryImpl implements PrivateKeyRegistry {
             factory.setPassword(privateKeyChooser.getPassword());
             try {
                 factory.afterPropertiesSet();
-                return (PrivateKey) factory.getObject();
+                PrivateKey privateKey = (PrivateKey) factory.getObject();
+
+                if (privateKey != null) {
+                    cache.put(cacheKey, privateKey);
+                }
+                return privateKey;
             } catch (Exception e) {
                 throw new PrivateKeyException("error initializing private key factory bean", e);
             }
         }
 
         return null;
+    }
+
+    private final static class CacheKey {
+
+        private String keyStoreName;
+        private String privateKeyAlias;
+
+        private CacheKey(String keyStoreName, String privateKeyAlias) {
+            this.keyStoreName = keyStoreName;
+            this.privateKeyAlias = privateKeyAlias;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            CacheKey cacheKey = (CacheKey) o;
+            return !(keyStoreName != null ? !keyStoreName.equals(cacheKey.keyStoreName) : cacheKey.keyStoreName != null) && !(privateKeyAlias != null ? !privateKeyAlias.equals(cacheKey.privateKeyAlias) : cacheKey.privateKeyAlias != null);
+        }
+
+        @Override
+        public int hashCode() {
+            int result = keyStoreName != null ? keyStoreName.hashCode() : 0;
+            result = 31 * result + (privateKeyAlias != null ? privateKeyAlias.hashCode() : 0);
+            return result;
+        }
+
+        @Override
+        public String toString() {
+            final StringBuilder sb = new StringBuilder();
+            sb.append("CacheKey");
+            sb.append("{keyStoreName='").append(keyStoreName).append('\'');
+            sb.append(", privateKeyAlias='").append(privateKeyAlias).append('\'');
+            sb.append('}');
+            return sb.toString();
+        }
     }
 }
--- src/main/java/com/google/code/springcryptoutils/core/key/PublicKeyRegistryImpl.java
+++ src/main/java/com/google/code/springcryptoutils/core/key/PublicKeyRegistryImpl.java
@@ -5,16 +5,27 @@ import com.google.code.springcryptoutils.core.keystore.KeyStoreRegistry;
 
 import java.security.KeyStore;
 import java.security.PublicKey;
+import java.util.HashMap;
+import java.util.Map;
 
 public class PublicKeyRegistryImpl implements PublicKeyRegistry {
 
     private KeyStoreRegistry keyStoreRegistry;
+
+    private Map<CacheKey, PublicKey> cache = new HashMap<CacheKey, PublicKey>();
 
     public void setKeyStoreRegistry(KeyStoreRegistry keyStoreRegistry) {
         this.keyStoreRegistry = keyStoreRegistry;
     }
 
     public PublicKey get(KeyStoreChooser keyStoreChooser, PublicKeyChooser publicKeyChooser) {
+        CacheKey cacheKey = new CacheKey(keyStoreChooser.getKeyStoreName(), publicKeyChooser.getAlias());
+        PublicKey retrievedPublicKey = cache.get(cacheKey);
+
+        if (retrievedPublicKey != null) {
+            return retrievedPublicKey;
+        }
+
         KeyStore keyStore = keyStoreRegistry.get(keyStoreChooser);
 
         if (keyStore != null) {
@@ -23,7 +34,12 @@ public class PublicKeyRegistryImpl implements PublicKeyRegistry {
             factory.setAlias(publicKeyChooser.getAlias());
             try {
                 factory.afterPropertiesSet();
-                return (PublicKey) factory.getObject();
+                PublicKey publicKey = (PublicKey) factory.getObject();
+
+                if (publicKey != null) {
+                    cache.put(cacheKey, publicKey);
+                }
+                return publicKey;
             } catch (Exception e) {
                 throw new PublicKeyException("error initializing the public key factory bean", e);
             }
@@ -31,4 +47,41 @@ public class PublicKeyRegistryImpl implements PublicKeyRegistry {
 
         return null;
     }
+
+    private final static class CacheKey {
+
+        private String keyStoreName;
+        private String publicKeyAlias;
+
+        private CacheKey(String keyStoreName, String publicKeyAlias) {
+            this.keyStoreName = keyStoreName;
+            this.publicKeyAlias = publicKeyAlias;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            CacheKey cacheKey = (CacheKey) o;
+            return !(keyStoreName != null ? !keyStoreName.equals(cacheKey.keyStoreName) : cacheKey.keyStoreName != null) && !(publicKeyAlias != null ? !publicKeyAlias.equals(cacheKey.publicKeyAlias) : cacheKey.publicKeyAlias != null);
+        }
+
+        @Override
+        public int hashCode() {
+            int result = keyStoreName != null ? keyStoreName.hashCode() : 0;
+            result = 31 * result + (publicKeyAlias != null ? publicKeyAlias.hashCode() : 0);
+            return result;
+        }
+
+        @Override
+        public String toString() {
+            final StringBuilder sb = new StringBuilder();
+            sb.append("CacheKey");
+            sb.append("{keyStoreName='").append(keyStoreName).append('\'');
+            sb.append(", publicKeyAlias='").append(publicKeyAlias).append('\'');
+            sb.append('}');
+            return sb.toString();
+        }
+    }
+
 }
