151455ced2382019f565b428a8c6d0564fc21592
Mirko Caserta <mirko.caserta@gmail.com>
2021-01-07 00:04:24 +0100

    sonar cloud fixes #2

--- src/test/java/com/springcryptoutils/keystore/spring/ClasspathKeystoreTest.java
+++ src/test/java/com/springcryptoutils/keystore/spring/ClasspathKeystoreTest.java
@@ -14,7 +14,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @SpringJUnitConfig
-public class ClasspathKeystoreTest {
+class ClasspathKeystoreTest {
 
     @Autowired
     private KeyStore keystore;
